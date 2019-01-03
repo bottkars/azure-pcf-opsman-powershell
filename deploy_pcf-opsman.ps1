4df817f79880d2ece686c1231702e1965d6b033e
bottkars <Karsten.Bott@bottnet.de>
2019-01-03 17:56:25 +0100

    get back storage accounts

--- deploy_pcf-opsman.ps1
+++ deploy_pcf-opsman.ps1
@@ -76,7 +76,7 @@
     [Parameter(ParameterSetName = "1", Mandatory = $false)][ValidateSet('green', 'blue')]$deploymentcolor = "green",
     [ipaddress]$subnet = "10.0.0.0",
     $downloadpath = "$($HOME)/Downloads",
-    [switch]$useManagedDisks=$true,
+    [switch]$useManagedDisks,
     [Parameter(ParameterSetName = "1", Mandatory = $false)]
     [ValidateNotNullOrEmpty()]
     [ValidateSet('AzureCloud', 'AzureStack')]$Environment = "AzureStack"
