a03dd2003699549f602a5f736b7887f4b58f2079
bottkars <karsten.bott@bottnet.de>
2019-01-04 13:05:50 +0100

    omcli

--- prepare_utils.ps1
+++ prepare_utils.ps1
@@ -1,5 +1,8 @@
 ### prepare utils
+Write-Host "installing pivposh"
 Install-Module pivposh -scope CurrentUser -Force
 $omdir = New-Item -ItemType Directory "$HOME/om"
+Write-Host "installing omcli to $($omdir.fullname)/om.exe"
+
 Invoke-WebRequest -UseBasicParsing -Uri https://github.com/pivotal-cf/om/releases/download/0.48.0/om-windows.exe -OutFile "$($omdir.fullname)/om.exe"
 unblock-file "$($omdir.fullname)/om.exe"
