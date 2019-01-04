654b2fb1b8a49ad3a57a0196e7fde0c83d2dbb8f
bottkars <karsten.bott@bottnet.de>
2019-01-04 13:04:10 +0100

    om tools

--- prepare_utils.ps1
+++ prepare_utils.ps1
@@ -1,4 +1,5 @@
 ### prepare utils
 Install-Module pivposh -scope CurrentUser -Force
-Invoke-WebRequest -UseBasicParsing -Uri https://github.com/pivotal-cf/om/releases/download/0.48.0/om-windows.exe -OutFile $HOME/om/om.exe
-unblock-file $HOME/om/om.exe
+$omdir = New-Item -ItemType Directory "$HOME/om"
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/pivotal-cf/om/releases/download/0.48.0/om-windows.exe -OutFile "$($omdir.fullname)/om.exe"
+unblock-file "$($omdir.fullname)/om.exe"
