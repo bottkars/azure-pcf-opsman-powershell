95874d8d7eb2356a36e854e4bb0aac92028666ae
bottkars <karsten.bott@bottnet.de>
2019-01-15 16:19:14 +0100

    new version of create certs

---
+++ createcert_old.ps1
@@ -0,0 +1,36 @@
+
+
+"
+[req]
+default_bits = 2048
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+
+[ dn ]
+C=DE
+ST=Hessen
+L=Taunusstein
+O=Karsten Bott
+OU=DEMO
+CN = *.$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME
+
+[ req_ext ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = *.system.$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME
+DNS.2 = *.login.system.$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME
+DNS.3 = *.uaa.system.$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME
+DNS.4 = *.apps.$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME
+DNS.5 = *.$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME
+" | set-content "$HOME/$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME.cnf"
+
+C:\OpenSSL-Win64\bin\openssl.exe req -x509 `
+  -newkey rsa:2048 `
+  -nodes `
+  -days 365 `
+  -keyout "$HOME/$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME.key" `
+  -out "$HOME/$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME.cert" `
+  -config "$HOME/$PCF_SUBDOMAIN_NAME.$PCF_DOMAIN_NAME.cnf"
