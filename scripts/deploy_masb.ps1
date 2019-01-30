ff5ead4c74b97a69d24d67ef4db0fbf060f95575
PCF Developer <pcfuser@labbuildr.com>
2019-01-30 19:22:06 +0100

    eula

--- scripts/deploy_masb.ps1
+++ scripts/deploy_masb.ps1
@@ -42,7 +42,7 @@ Write-Host "Getting Release for $slug_id $PCF_MASB_VERSION"
 $piv_release = Get-PIVRelease -id $slug_id | where version -Match $PCF_MASB_VERSION | Select-Object -First 1
 $piv_release_id = $piv_release | Get-PIVFileReleaseId
 $access_token = Get-PIVaccesstoken -refresh_token $PCF_PIVNET_UAA_TOKEN
-Confirm-PIVEula -access_token $access_token -slugid 82 -id 290314
+$eula = Confirm-PIVEula -access_token $access_token -slugid 82 -id 290314
 
 
 Write-Host "Accepting EULA for $slug_id $PCF_MASB_VERSION"
