d38d387f0dbad110bab324557914dca9a091903b
PCF Developer <pcfuser@labbuildr.com>
2019-01-30 19:47:25 +0100

    apply stemcell

--- scripts/deploy_masb.ps1
+++ scripts/deploy_masb.ps1
@@ -68,6 +68,7 @@ if (($force_product_download.ispresent) -or (!(test-path "$($output_directory.Fu
 $download_file = get-content "$($output_directory.FullName)/download-file.json" | ConvertFrom-Json
 $TARGET_FILENAME = $download_file.product_path
 $STEMCELL_FILENAME = $download_file.stemcell_path
+$STEMCELL_VERSION =  $download_file.stemcell_version
 
 
 Write-Host "importing $TARGET_FILENAME into OpsManager"
@@ -99,7 +100,10 @@ om --skip-ssl-validation `
     --product-name $PRODUCT_NAME `
     --product-version $VERSION
 
-
+    om --skip-ssl-validation `
+    apply-stemmcell `
+    --product $PRODUCT_NAME `
+    --stemcell $STEMCELL_VERSION
 
 "
 product_name: $PRODUCT_NAME
