704cb060066e756876e03b546390c26d2023c940
PCF Developer <pcfuser@labbuildr.com>
2019-02-09 09:28:05 +0100

    tile deployer

---
+++ docs/example1.md
@@ -0,0 +1,16 @@
+# Installing PAS with Default Settings on ASDK
+
+## Desired State
+
+This is the Goal State
+
+
+## Command to run
+
+```Powershell
+
+```
+
+
+
+![image](https://user-images.githubusercontent.com/8255007/52518133-3df04480-2c46-11e9-8f7c-0b27453a90fb.png)
---
+++ scripts/tile_deployer_preview.ps1
@@ -0,0 +1,70 @@
+#requires -module pivposh
+param(
+    [Parameter(ParameterSetName = "install", Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('mysql', 'rabbitmq', 'spring', 'redis', 'apm', 'dataflow', 'healthwatch', 'masb')]
+    [string[]]$tiles,
+    [Parameter(Mandatory = $true)]	
+    [Validatescript( {Test-Path -Path $_ })]
+    $DIRECTOR_CONF_FILE,
+    [Parameter(Mandatory = $false)]	
+    [switch]$DO_NOT_APPLY,
+    [Parameter(Mandatory = $false)]
+    [switch]$do_not_configure_azure_DB
+)
+Push-Location $PSScriptRoot
+
+
+if ($tiles -contains 'spring') {
+    $tiles = ('mysql', 'rabbitmq', 'spring') + $tiles
+    $tiles = $tiles | Select-Object -Unique
+    $compute_instances = 2
+}
+if ($tiles -contains 'dataflow') {
+    $tiles = ('mysql', 'rabbitmq', 'redis', 'dataflow') + $tiles
+    $tiles = $tiles | Select-Object -Unique
+}
+if ($tiles -contains 'masb') {
+    if (!$env_vars.AZURE_CLIENT_ID `
+            -or !$env_vars.AZURE_CLIENT_SECRET `
+            -or !$env_vars.AZURE_REGION `
+            -or !$env_vars.AZURE_SUBSCRIPTION_ID `
+            -or !$env_vars.AZURE_TENANT_ID `
+            -or !$env_vars.AZURE_DATABASE_ENCRYPTION_KEY  
+    ) {
+        Write-Host -ForegroundColor White "Microsoft Azure Service Broker needs
+                AZURE_CLIENT_ID 
+                AZURE_CLIENT_SECRET 
+                AZURE_REGION 
+                AZURE_SUBSCRIPTION_ID 
+                AZURE_TENANT_ID
+                AZURE_DATABASE_ENCRYPTION_KEY
+to be set correctly with role `contributor`for the Service Principal at the AZURE subscription level"
+        Pop-Location
+        break
+    }
+
+  
+} 
+Write-Host -ForegroundColor White -NoNewline "Going to deploy PCF $PASTYPE with the Following Tiles: "
+Write-Host -ForegroundColor Green  "$($tiles -join ",")"
+
+
+
+ForEach ($tile in $tiles) {
+    $StopWatch_deploy = New-Object System.Diagnostics.Stopwatch
+    $StopWatch_deploy.Start()
+    if ($tile -match $tiles[-1]) {
+        $command = "$ScriptHome/scripts/deploy_$($tile).ps1 -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE"
+        Write-Host "Calling $command" 
+        # Invoke-Expression -Command $Command | Tee-Object -Append -FilePath "$($HOME)/$($tile)-$(get-date -f yyyyMMddhhmmss).log"
+    }
+    else {
+        $command = "$ScriptHome/scripts/deploy_$($tile).ps1 -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE -DO_NOT_APPLY"
+        Write-Host "Calling $command" 
+        # Invoke-Expression -Command $Command | Tee-Object -Append -FilePath "$($HOME)/$($tile)-$(get-date -f yyyyMMddhhmmss).log"
+
+    }    
+    $StopWatch_deploy.Stop()
+    $DeployTimes += "$tile deployment took $($StopWatch_deploy.Elapsed.Hours) hours, $($StopWatch_deploy.Elapsed.Minutes) minutes and  $($StopWatch_deploy.Elapsed.Seconds) seconds"
+}
