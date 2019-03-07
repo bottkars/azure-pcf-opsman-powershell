f0ffb9b65e7f3794d7913b36419bb2a2c06ca89a
PCF Developer <pcfuser@labbuildr.com>
2019-03-07 07:54:59 +0100

    added healthwatch to uaa admin

--- scripts/assign_uaa_admin.ps1
+++ scripts/assign_uaa_admin.ps1
@@ -3,7 +3,10 @@ param(
     [string]$user,
     [Parameter(Mandatory = $false)]
     [ValidateSet('add','delete')]
-    $assignment = 'add'	
+    $assignment = 'add'	,
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('system','healthwatch')]
+    [alias('app')]$space
 )
 <#
 .$PSScriptRoot/uaa_login.ps1 -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE
@@ -13,8 +16,20 @@ $Userlist = ($USERS -replace "RESPONSE BODY:" -replace ">" |  ConvertFrom-Json).
 $Wavefront_user = ($Userlist | where userName -match wavefront-nozzle)
 #>
 $assignment = $assignment.tolower()
+switch ($space)
+{
+    'healthwatch'
+{
+$roles = ('healthwatch.read','healthwatch.admin')
+}
+    'system'
+{
 $roles = ('cloud_controller.admin','uaa.admin','scim.read','scim.write')
+}
+}
 foreach ($role in $roles) {
     Write-Host -ForegroundColor White -NoNewline "running member $assignment  $role for  $user ==> "
     uaac member $assignment $role $user
 }
+
+
