3054be3c30f8c71fb7adc32d8373770f99c4cf95
PCF Developer <pcfuser@labbuildr.com>
2019-03-16 07:34:01 +0100

    prodname

--- scripts/deploy_tile.ps1
+++ scripts/deploy_tile.ps1
@@ -152,7 +152,7 @@ $PRODUCTS = $(om --skip-ssl-validation `
 $PRODUCT = $PRODUCTS | where-object name -Match $PRODUCT_NAME | Sort-Object -Descending -Property version | Select-Object -First 1
 $PRODUCT_NAME = $PRODUCT.name
 $VERSION = $PRODUCT.version
-Write-Verbose "we now have $PRODUCT_NAME $PCF_VERSION"
+Write-Verbose "we now have $PRODUCT_NAME VERSION"
 om --skip-ssl-validation `
     deployed-products
 # 2.  Stage using om cli
