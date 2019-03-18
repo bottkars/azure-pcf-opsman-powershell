0756815d10808cc3bc0e0b3dda397e56acc90ded
PCF Developer <pcfuser@labbuildr.com>
2019-03-18 18:24:24 +0100

    deployed

---
+++ scripts/deployed_products.ps1
@@ -0,0 +1,30 @@
+
+    
+param(
+    [Parameter(Mandatory = $true)]
+    [Validatescript( {Test-Path -Path $_ })]
+    $DIRECTOR_CONF_FILE
+)
+
+Push-Location $PSScriptRoot
+$director_conf = Get-Content $DIRECTOR_CONF_FILE | ConvertFrom-Json
+$OM_Target = $director_conf.OM_TARGET
+# setting the env
+$env_vars = Get-Content $HOME/env.json | ConvertFrom-Json
+$env:OM_Password = $env_vars.OM_Password
+$env:OM_Username = $env_vars.OM_Username
+$env:OM_Target = $OM_Target
+$env:Path = "$($env:Path);$HOME/OM"
+try
+{$PRODUCTS=$(om --skip-ssl-validation `
+  deployed-products `
+    --format json) | ConvertFrom-Json
+}
+catch
+{
+    Write-Host "There was an error"
+    Pop-Location
+    Break
+}
+Write-Output $PRODUCTS
+Pop-Location
