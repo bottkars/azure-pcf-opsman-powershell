248173786e6dbd64d6f7f0dc5bd1d3c128668982
bottkars <karsten.bott@bottnet.de>
2019-04-11 08:51:57 +0200

    requires

--- scripts/deploy_tile.ps1
+++ scripts/deploy_tile.ps1
@@ -1,4 +1,4 @@
-#requires -module pivposh -version 0.5
+#Requires -Modules @{ModuleName="pivposh";ModuleVersion="0.5"}
 param(
     [Parameter(ParameterSetName = "no_apply", Mandatory = $true)]
     [switch]$DO_NOT_APPLY,
