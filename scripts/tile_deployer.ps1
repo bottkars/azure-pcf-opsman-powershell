67fb0d6dc77cd0b390eaf46560dd649276972ddb
bottkars <karsten.bott@bottnet.de>
2019-04-11 12:00:12 +0200

    no apply on tile deployer

--- scripts/tile_deployer.ps1
+++ scripts/tile_deployer.ps1
@@ -19,7 +19,7 @@ param(
         'minio-internal-blobstore')] 
     [string[]]$tiles,
     [Parameter(Mandatory = $true)]	
-    [Validatescript( {Test-Path -Path $_ })]
+    [Validatescript( { Test-Path -Path $_ })]
     $DIRECTOR_CONF_FILE,
     [Parameter(Mandatory = $false)]	
     [switch]$DO_NOT_APPLY,
@@ -28,17 +28,17 @@ param(
 )
 $ScriptDir = "$PSScriptRoot"
 Push-Location $PSScriptRoot
-New-Item -ItemType Directory -Path "$($HOME)/pcfdeployer/logs" -Force | out-null
+New-Item -ItemType Directory -Path "$($HOME)/pcfdeployer/logs" -Force | Out-Null
 #$director_conf = Get-Content $DIRECTOR_CONF_FILE | ConvertFrom-Json
 $env_vars = Get-Content $HOME/env.json | ConvertFrom-Json
 
 
-if ($tiles -contains 'p-spring-cloud-services') {
-    $tiles = ('pivotal-mysql', 'p-rabbitmq', 'p-spring-cloud-services') + $tiles
+if ($tiles -contains 'p-spring') {
+    $tiles = ('pivotal-mysql', 'p-rabbitmq', 'p-spring') + $tiles
     $tiles = $tiles | Select-Object -Unique
 }
-if ($tiles -contains 'p-dataflow') {
-    $tiles = ('pivotal-mysql', 'p-rabbitmq', 'p-redis', 'p-dataflow') + $tiles
+if ($tiles -contains 'dataflow') {
+    $tiles = ('pivotal-mysql', 'p-rabbitmq', 'p-redis', 'dataflow') + $tiles
     $tiles = $tiles | Select-Object -Unique
 }
 if ($tiles -contains 'p-event-alerts') {
@@ -77,13 +77,20 @@ ForEach ($tile in $tiles) {
     $StopWatch_Tile_deploy = New-Object System.Diagnostics.Stopwatch
     $StopWatch_Tile_deploy.Start()
     if ($tile -match $tiles[-1]) {
-        $command = "$ScriptDir/deploy_tile.ps1 -tile $tile -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE"
+        if ($DO_NOT_APPLY.IsPresent) {
+            $command = "$ScriptDir/deploy_tile.ps1 -tile $tile -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE -DO_NOT_APPLY"
+
+        }
+        else {              
+            $command = "$ScriptDir/deploy_tile.ps1 -tile $tile -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE"
+      
+        }    
     }
     else {
         $command = "$ScriptDir/deploy_tile.ps1 -tile $tile -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE -DO_NOT_APPLY"
     }
     Write-Host "Calling $command" 
-    Invoke-Expression -Command $Command | Tee-Object -Append -FilePath "$($HOME)/pcfdeployer/logs/$($tile)-$(get-date -f yyyyMMddhhmmss).log"
+    Invoke-Expression -Command $Command | Tee-Object -Append -FilePath "$($HOME)/pcfdeployer/logs/$($tile)-$(Get-Date -f yyyyMMddhhmmss).log"
     $StopWatch_Tile_deploy.Stop()
     $DeployTimes += "$tile deployment took $($StopWatch_Tile_deploy.Elapsed.Hours) hours, $($StopWatch_Tile_deploy.Elapsed.Minutes) minutes and  $($StopWatch_Tile_deploy.Elapsed.Seconds) seconds"
 }
