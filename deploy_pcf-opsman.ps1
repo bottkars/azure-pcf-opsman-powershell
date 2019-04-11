2c4f6d8a05f8de9a0f074024fce30953fc3ba294
bottkars <karsten.bott@bottnet.de>
2019-04-11 07:30:38 +0200

    spring

--- deploy_pcf-opsman.ps1
+++ deploy_pcf-opsman.ps1
@@ -383,18 +383,18 @@ Write-Host
 if ($PsCmdlet.ParameterSetName -eq "install") {
     if ($tiles) {
         [switch]$PAS_AUTOPILOT = $true
-        if ($tiles -contains 'spring') {
-            $tiles = ('mysql', 'rabbitmq', 'spring') + $tiles
+        if ($tiles -contains 'p-spring-cloud-services') {
+            $tiles = ('pivotal-mysql', 'p-rabbitmq', 'p-spring-cloud-services') + $tiles
             $tiles = $tiles | Select-Object -Unique
             if ($compute_instances -lt 2) {
                 $compute_instances = 2
             }
         }
-        if ($tiles -contains 'dataflow') {
-            $tiles = ('mysql', 'rabbitmq', 'redis', 'dataflow') + $tiles
+        if ($tiles -contains 'p-dataflow') {
+            $tiles = ('pivotal-mysql', 'p-rabbitmq', 'p-redis', 'p-dataflow') + $tiles
             $tiles = $tiles | Select-Object -Unique
         }
-        if ($tiles -contains 'masb') {
+        if ($tiles -contains 'azure-service-broker') {
             if (!$env_vars.AZURE_CLIENT_ID `
                     -or !$env_vars.AZURE_CLIENT_SECRET `
                     -or !$env_vars.AZURE_REGION `
@@ -688,7 +688,19 @@ if (!$OpsmanUpdate) {
             else {
                 $command = "$ScriptDir/init_om.ps1 -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE"    
             }
-            
+            switch ($branch)
+            {
+                '2.5' {
+                    $stemcell = '250'
+                }
+                '2.4' {
+                    $stemcell = '170'
+                } 
+                default {
+                    $stemcell = "97"
+                }   
+
+            }
             Write-Host "Calling $command" 
             Invoke-Expression -Command $Command | Tee-Object -Append -FilePath "$($HOME)/pcfdeployer/logs/init-om-$(get-date -f yyyyMMddhhmmss).log"
             $StopWatch_deploy_opsman.Stop()
@@ -697,7 +709,7 @@ if (!$OpsmanUpdate) {
             if ($PAS_AUTOPILOT.IsPresent) {
                 $StopWatch_deploy_stemcells = New-Object System.Diagnostics.Stopwatch
                 $StopWatch_deploy_stemcells.Start()
-                $command = "$ScriptDir/get-lateststemcells.ps1 -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE"
+                $command = "$ScriptDir/get-lateststemcells.ps1 -DIRECTOR_CONF_FILE $DIRECTOR_CONF_FILE -families $stemcell"
                 Write-Host "Calling $command" 
                 Invoke-Expression -Command $Command | Tee-Object -Append -FilePath "$($HOME)/pcfdeployer/logs/stemcells-$(get-date -f yyyyMMddhhmmss).log"
                 $StopWatch_deploy_stemcells.Stop()
